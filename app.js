48a7b69366181e63f317d3f98fe05a307925c90f
huan ton <huanton123456789@gmail.com>
2021-07-06 16:50:31 +0700

    update 15

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var config = require("config");
 var bodyParser = require("body-parser");
-var session = require("cookie-session");
+var session = require("express-session");
 var auth = require('./apps/middlewares/auth');
 
 var app = express();
 //body parser
-app.use(express.json());
+app.use(bodyParser.json());
 
 app.set("views", __dirname + "/apps/views");
 app.set('view engine', 'ejs');
@@ -15,21 +15,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
-
-var sess = {
+app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
-    cookie: {}
-}
+}));
 
-if (app.get('env') === 'production') {
-    app.set('trust proxy', 1) // trust first proxy
-    sess.cookie.secure = true // serve secure cookies
-}
-
-app.use(session(sess))
-    //check login
+//check login
 app.use(auth);
 
 //static folder
