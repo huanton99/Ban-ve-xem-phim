7732739369d753c9f8577c60abcd3a7c27b231c2
huan ton <huanton123456789@gmail.com>
2021-07-06 17:35:39 +0700

    update 20

--- apps/common/database.js
+++ apps/common/database.js
@@ -1,6 +1,6 @@
 var Sequelize = require("sequelize");
 
-var url = 'postgres://postgres:1234@localhost:5432/lottecinema'
+var url = 'postgres://ejeweohykqcuvg:8a653f7e52496b2e8e3c336f932bd8ae38d667e38dbf3c0dae39fc9ebb7be638@ec2-52-5-1-20.compute-1.amazonaws.com:5432/dfj4t68do50df0'
 var database = new Sequelize(url);
 
 module.exports = database;
