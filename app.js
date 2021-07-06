0be9b10172a8e7fb27601af27fb6c607d944c0fd
huan ton <huanton123456789@gmail.com>
2021-07-06 11:52:15 +0700

    update

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var config = require("config");
+var bodyParser = require("body-parser");
+var session = require("cookie-session");
+var auth = require('./apps/middlewares/auth');
+
+var app = express();
+//body parser
+app.use(express.json());
+
+app.set("views", __dirname + "/apps/views");
+app.set('view engine', 'ejs');
+
+//middlewares
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+//check login
+app.use(auth);
+
+//static folder
+app.use("/static", express.static(__dirname + "/public"));
+
+var controllers = require(__dirname + "/apps/controllers");
+
+app.use(controllers);
+
+var port = config.get("server.port") || process.env.PORT;
+app.listen(port, "0.0.0.0", function() {
+    console.log(`Server listening on port ${port}`);
+});
---
+++ apps/mailer/cf_mailer.js
@@ -0,0 +1,11 @@
+module.exports = {
+    supportMail: "tdhuan99@gmail.com",
+    email: 'tdhuan99@gmail.com',
+    pass: "01697509396",
+    siteName: "TOP CINEMA",
+    password: '01697509396',
+
+    host: 'smtp.gmail.com',
+    port: '587',
+    service: 'gmail'
+}
